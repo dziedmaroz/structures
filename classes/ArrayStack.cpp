bafd63242abfa779305fbd66de7fa937c587abf0
DziedMaroz <ilya.arkhanhelsky@gmail.com>
2011-04-07 21:53:52 +0300

    + small fixes

--- classes/ArrayStack.cpp
+++ classes/ArrayStack.cpp
@@ -3,14 +3,16 @@
 #include <iostream>
 using namespace std;
 
-template <class T> ArrayStack<T>::ArrayStack (int size)
+template<class T>
+        ArrayStack<T>::ArrayStack (int size)
 {
     arr_= new T[size];
     size_ = size;
     top_=0;
 }
 
-template <class T> ArrayStack<T>::ArrayStack (T *arr, int arrSize, int stackSize, bool reverse)
+template<class T>
+        ArrayStack<T>::ArrayStack (T *arr, int arrSize, int stackSize, bool reverse)
 {
     arr_ = new T [stackSize];
     size_=stackSize;
@@ -27,7 +29,8 @@ template <class T> ArrayStack<T>::ArrayStack (T *arr, int arrSize, int stackSize
     }
 }
 
-template <class T> ArrayStack<T>::ArrayStack (ArrayStack &orig)
+template<class T>
+        ArrayStack<T>::ArrayStack (ArrayStack &orig)
 {
     arr_ = new T [orig.size_];
     size_=orig.size_;
@@ -37,42 +40,49 @@ template <class T> ArrayStack<T>::ArrayStack (ArrayStack &orig)
         this->arr_[i]=orig.arr_[i];
 }
 
-template <class T> bool  ArrayStack<T>::isEmpty ()
+template<class T>
+        bool  ArrayStack<T>::isEmpty ()
 {
     return top_==0?true:false;
 }
 
-template <class T> bool ArrayStack<T>::isFull ()
+template<class T>
+        bool ArrayStack<T>::isFull ()
 {
     return top_==size_?true:false;
 }
 
-template <class T> void ArrayStack<T>::push (T item)
+template<class T>
+        void ArrayStack<T>::push (T item)
 {
     if (isFull ()) throw (StructExceptions("Stack is full!"));
-    arr_[top_]=item;
-    top_++;
+    arr_[top_++]=item;
+
 }
 
-template <class T> T ArrayStack<T>::pop ()
+template<class T>
+        T ArrayStack<T>::pop ()
 {
     if (isEmpty ()) throw (StructExceptions("Stack is empty!"));
     return arr_[top_--];
 }
 
-template <class T> T ArrayStack<T>::peek ()
+template<class T>
+        T ArrayStack<T>::peek ()
 {
     return arr_[top_];
 }
 
-template <class T> void ArrayStack<T>::print ()
+template<class T>
+        void ArrayStack<T>::print ()
 {
     for (int i=0;i<top_;i++)
         cout<<arr_[i]<<" \t";
     cout<<"\n";
 }
 
-template <class T>  ArrayStack<T>::~ArrayStack ()
+template<class T>
+        ArrayStack<T>::~ArrayStack ()
 {
     delete [] arr_;
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
-#include <stdio.h>
-#include <cstdlib>
 #include "classes/ArrayStack.h"
 int main(int argc, char *argv[])
 {
     ArrayStack<int> stack (5);
     for (int i=0;i<5;i++)
-        stack.push (i);
+        stack.push (i);        
     stack.print ();
     return 0;
 }
